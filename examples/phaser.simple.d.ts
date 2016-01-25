b3854d418f9d6d8cb86561b5deb723e2a10352c5
Ben Jackman <ben@jackman.biz>
2016-01-25 16:52:42 -0600

    Added some examples
    Signed-off-by: Ben Jackman <ben@jackman.biz>

---
+++ examples/phaser.simple.d.ts
@@ -0,0 +1,15 @@
+
+// Type definitions for Phaser 2.3.0 RC1 2015-03-24
+// Project: https://github.com/photonstorm/phaser
+
+declare class Phaser {
+    static VERSION: string;
+    static GAMES: Phaser.Game[];
+
+    static AUTO: number;
+
+    foo(a: number|string) : number|string;
+}
+
+declare module Phaser {
+}
